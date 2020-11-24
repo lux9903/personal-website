1129de5663be7c0fd00df02249e62ec6377e1ce7
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-11-24 19:13:04 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+import Footer from './pages/Footer';
+import Header from './pages/Header';
+
+const App = () => {
+    return (
+        <Router>
+            <Route path='/:page' component={Header} />
+            <Route exact path='/' component={Header} />
+            <Switch>
+                <Route exact path='/' component={About} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/projects' component={Projects} />
+                <Route exact path='/contact' component={Contact} />
+                <Route path='*' component={Error} />
+            </Switch>
+            <Footer />
+        </Router>
+    );
+};
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const About = () => {
+    return (
+        <div>
+        <p>About us</p>
+        </div>
+    );
+};
+
+export default About;
